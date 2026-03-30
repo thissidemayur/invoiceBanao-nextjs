0f7734e5c3a27aa8555c7fc717bfa4d4fb3033e8
thissidemayur <thissidemayur@gmail.com>
2026-03-30 13:23:28 +0530

    update: update nextjs and other packages

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["ik.imagekit.io"]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ik.imagekit.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
